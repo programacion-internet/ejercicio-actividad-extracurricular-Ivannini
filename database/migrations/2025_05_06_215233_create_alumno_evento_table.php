8b6d3974689f3f4448547823391f39ef4ce49b1c
Ivannini <ivan.nudelstejer1301@alumnos.udg.mx>
2025-05-07 14:50:43 -0600

    Actualizar cambios

--- database/migrations/2025_05_06_215233_create_alumno_evento_table.php
+++ database/migrations/2025_05_06_215233_create_alumno_evento_table.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('evento_user', function (Blueprint $table) {
+        Schema::create('alumno_evento', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('evento_id')->constrained('eventos');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('evento_user');
+        Schema::dropIfExists('alumno_evento');
     }
 };
